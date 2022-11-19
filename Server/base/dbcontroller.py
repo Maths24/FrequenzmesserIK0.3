6f9bd0fdec2c3ab57986353c693c0f6cbb9b53ef
Maths24 <matthi24.m@web.de>
2022-11-19 15:06:14 +0100

    .

--- Server/base/dbcontroller.py
+++ Server/base/dbcontroller.py
@@ -1,17 +1,18 @@
 import mysql.connector
 
+
 class DBController:
     def __init__(self) -> None:
         print("database started")
-        #Verbindung zu Datenbank herstellen
+        # Verbindung zu Datenbank herstellen
         mydb = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="himbeertorteDB",
-        database="mydatabase")
+            host="localhost",
+            user="root",
+            password="himbeertorteDB",
+            database="kundenzaehler")
 
         self.mycursor = mydb.cursor()
-   
+
     def query(self, query=""):
         self.mycursor.execute(query)
         return self.mycursor.fetchall()
