5a26ad361418839b8d026e8b96c0a6b59d7ce76e
Maths24 <matthi24.m@web.de>
2022-11-19 16:08:51 +0100

    .

--- Server/base/views.py
+++ Server/base/views.py
@@ -158,7 +158,7 @@ def chart_vonbis(start, end):
     print(start, end)
     # Richtige Daten eintragen
     data = dbc.query(
-        f"select kundenzahl from daten where Datum BETWEEN {start} AND {end}")
+        f"select kundenzahl from daten where Datum BETWEEN 01-02-2000 AND 11-11-2022")
    # data = dbc.query(f"select kundenzahl from daten ")
     return data
 
