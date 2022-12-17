86bc723814f792be296a3ff5c588505825f60e3b
Maths24 <matthi24.m@web.de>
2022-12-17 14:56:34 +0100

    ”.“

--- KI/database/dbManager.py
+++ KI/database/dbManager.py
@@ -5,18 +5,18 @@ class DBManager:
 
     def __init__(self) -> None:
         print("database started")
-
-    def addToDB(self, amount):
-        mydb = mysql.connector.connect(
+        self.mydb = mysql.connector.connect(
             host="localhost",
             user="root",
             password="himbeertorteDB",
             database="kundenzaehler"
         )
-        cursor = mydb.cursor()
+        self.cursor = self.mydb.cursor()
+
+    def addToDB(self, amount):
 
         sql = "INSERT INTO daten (kundenzahl) VALUES (%s)"
-        val = (amount)
-        cursor.execute(sql, val)
+        val = (str(amount))
+        self.cursor.execute(sql, val)
 
-        mydb.commit()
+        self.mydb.commit()
