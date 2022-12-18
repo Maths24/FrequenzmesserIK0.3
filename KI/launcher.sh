4b739975fdf6a31904fd4fa2c718c9079f674e78
Maths24 <matthi24.m@web.de>
2022-12-18 14:02:31 +0100

    ”.“

--- KI/launcher.sh
+++ KI/launcher.sh
@@ -3,6 +3,6 @@
 # navigate to home directory, then to this directory, then execute python script, then back home
 
 cd /
-cd home/pi/bbt
+cd home/pi/Skripte/FrequenzmesserIK0.3/KI/
 sudo python Run.py
 cd /
